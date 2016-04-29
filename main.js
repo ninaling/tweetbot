1d1eca749ded43c8df48b125d672dbe5832e7db1
thetoothless <ninaling@ucla.edu>
2016-04-29 00:39:57 -0700

    fixed react issues

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-var Tweet=React.createClass({
-  render: function(){
-    return(
-      <div className="tweet_container">
-        <span className="tweet_title">{this.props.itemNum}</span>
-        <div className='tweet_message'>{this.props.message}</div>
-      </div>
-    );
-  }
-});
-
 var items=['hello', 'what\'s up', 'how\'s it going'];
 var n=items.length;
+
+var Tweet=React.createClass({
+  render: function(){
+    var tweetsList=items.map(function(message, i){
+      return(
+        <div className="tweet_container">
+          <span className='tweet_title'>{i.toString()}</span>
+          <div className='tweet_message'>{message}</div>
+        </div>
+      );
+    });
+    return(<ul>{tweetsList}</ul>);
+  }
+});
 
 //var temp=0;
 //React.render(<Tweet itemNum={temp.toString()} message="hi"/>, document.getElementById('container'));
 
-for (var i=0; i<n; i++){
-  React.render(<Tweet itemNum={i.toString()} message={items[i]}/>, document.getElementById('container'));
-}
+React.render(<Tweet/>, document.getElementById('container'));
